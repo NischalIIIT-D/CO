7c2a90a58e69c3c9f4b6fb7e21c182311fbf0880
Saraaansh <saransh24508@iiitd.ac.in>
2025-02-19 22:08:17 +0530

    Update Assembler.py

--- SimpleAssembler/Assembler.py
+++ SimpleAssembler/Assembler.py
@@ -1,3 +1,10 @@
 import sys
 input_file = sys.argv[1]
 output_file = sys.argv[2]
+with open(input_file, 'r') as f:
+    list = f.readlines()
+    l = []
+    for i in list:
+        l.append((i.rstrip("\n").split()))
+with open(output_file, 'w') as f:
+    f.write("")
