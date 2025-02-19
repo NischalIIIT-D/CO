04f248cba08eb08b8ce2837fbe93fd6fceb3b1e9
Nischal Kant Singh <nischal24391@iiitd.ac.in>
2025-02-19 22:51:44 +0530

    Update Assembler.py

--- SimpleAssembler/Assembler.py
+++ SimpleAssembler/Assembler.py
@@ -8,3 +8,48 @@ with open(input_file, 'r') as f:
         l.append((i.rstrip("\n").split()))
 with open(output_file, 'w') as f:
     f.write("")
+    
+R_type = ["add", "sub","mul", "slt", "sltu", "xor", "sll", "srl", "or", "and"]
+I_type = ["lb", "lh", "lw", "ld", "addi", "sltiu", "jalr"]
+S_type = ["sb", "sh", "sw", "sd"]
+B_type = ["beq", "bne", "bge", "bgeu", "blt", "bltu"]
+U_type = ["auipc", "lui"]
+J_type = ["jal"]
+Bonus_type = ["rst","halt","rvrs"]
+
+label = {}
+
+regABItoBinary = {"zero": "00000",
+                  "ra": "00001",
+                  "sp": "00010",
+                  "gp": "00011",
+                  "tp": "00100",
+                  "t0": "00101",
+                  "t1": "00110",
+                  "t2": "00111",
+                  "s0": "01000", 
+                  "fp": "01000",
+                  "s1": "01001",
+                  "a0": "01010",
+                  "a1": "01011",
+                  "a2": "01100",
+                  "a3": "01101",
+                  "a4": "01110",
+                  "a5": "01111",
+                  "a6": "10000",
+                  "a7": "10001",
+                  "s2": "10010",
+                  "s3": "10011",
+                  "s4": "10100",
+                  "s5": "10101",
+                  "s6": "10110",
+                  "s7": "10111",
+                  "s8": "11000",
+                  "s9": "11001",
+                  "s10": "11010",
+                  "s11": "11011",
+                  "t3": "11100",
+                  "t4": "11101",
+                  "t5": "11110",
+                  "t6": "11111",
+                  }
